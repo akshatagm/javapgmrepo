ce21f9484d1a7891c109cdf4f524e9c1e3e590c0
akshata <akshatamanturi97@gmail.com>
2024-10-03 06:51:30 +0530

    ass7

--- Assignment7/EBook.java
+++ Assignment7/EBook.java
@@ -1,2 +1,21 @@
-package Assignment7;public class EBook {
+package Assignment7;
+
+public class EBook extends Book {
+
+    double filesize;
+
+    EBook(String title, String author, double price, int publicationYear) {
+        super(title, author, price, publicationYear);
+    }
+
+
+    public void displayInfo(){
+        System.out.println(filesize);
+    }
+
+    @Override
+    public double discountedPrice(double discountPercentage) {
+        return super.discountedPrice(discountPercentage);
+    }
 }
+
